55383e56c9f7be7af404207825a2b84a048efc3f
Sandeep Kaur <sandeepsidhu685@gmail.com>
2025-07-13 11:58:24 -0700

    Time: 5 ms (37.76%), Space: 19.3 MB (5.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums = list(enumerate(nums))
         nums.sort(key = lambda num: num[1])
-        print(nums)
         l=0
         r=len(nums)-1
         while r>l:
